511c7d6efd1e0e6fe7f2442921e90c0367289011
Dan Jones <djones.home@comcast.net>
2018-08-07 13:52:56 -0700

    Reworked tests

--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,23 @@
 // Require the built in 'assertion' library
 var assert = require('assert');
-var data = require('../tests/projectSettings')
-var es = require('../lib/profiles')
-process.env.CIDATA='/home/djones/ws/ciData/ciStack.json'
-
-es.getRole('vm','bastion')
-var fs = require('fs')
-showMeRoles()
+var prof = require('../lib/profiles')
+var data = require('../tests/profiles')
 
 // Create a test suite (group) called Math
-describe('Profile Settings', function() {
-    // Test getRole: 
-    it('should return a list', function(){
-      // Our actual test: 3*3 SHOULD EQUAL 9
-      assert.equal(es.getRole("vm",data), 3*3);
+describe('Roles x,y,z Profile Settings', function() {
+    // Test getRole x: 
+    it('x should return Profiles: default', function(){
+      assert.equal(prof.getRole('subject','x',data).ResolvedProfiles.join(), 'default');
+      });
+    // Test role y
+    it('y should return Profiles: b,c,d,default', function(){
+        assert.equal(prof.getRole('subject','y',data).ResolvedProfiles.join(), 'b,c,d,default');        assert.equal(prof.getRole('subject','y',data).ResolvedProfiles.join(), 'b,c,d,default');
     });
-    // Test Two: A string explanation of what we're testing
-    it('should return Profiles list', function(){
-      // Our actual test: (3-4)*8 SHOULD EQUAL -8
-      assert.equal(-8, (3-4)*8);
+        // Test role z
+    it('z should return Profiles: c,b,default', function(){
+        assert.equal(prof.getRole('subject','z',data).ResolvedProfiles.join(), 'c,b,default');
+
     });
+
 });
 
-function showMeRoles() {
-  fs.readdirSync(`${process.env.WORKSPACE}/ciData`).filter(fn=> /.*\.json$/.test(fn)).forEach( n => {
-    let j = JSON.parse(fs.readFileSync(n))
-    if (j.Project && j.Profiles && j.InstanceRoles) {
-      console.log('file:%s, Project %s\n', n, j.Project)
-
-      Object.keys(j.InstanceRoles).forEach( rn => { 
-        console.log(JSON.stringify(es.getRole('vm', rn, j),null, 2))})
-
-    }
-  })
-}
