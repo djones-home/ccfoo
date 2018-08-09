4b9da118f469b8a7c70bc4b9c2bc72e2f6fef6cf
Dan Jones <djones.home@comcast.net>
2018-08-09 13:17:02 -0700

    Broken/breaking argument parsing in commander, after adding caching.

--- bin/ccfoo-vm-show.js
+++ bin/ccfoo-vm-show.js
@@ -11,9 +11,9 @@ const domain = process.env.DOMAIN || config.DOMAIN || "";
 const username =  process.env.AZURE_USERNAME || config.AZURE_USERNAME || config.username || "";
 const password = process.env.AZURE_PASSWORD || config.AZURE_PASSWORD || config.password || "";
 const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID || config.AZURE_SUBSCRIPTION_ID || config.id ||"";
-
+const profiles = require('../lib/profiles')
 async function main() {
-  let cmdId = "listAll"
+  let cmdId = `${(profiles.env|| "az") + subject}listAll`
   let vms = cache.cGet(cmdId, 600 )
   if ( ! vms ) {
     const loginOptions = { environment: AzureEnvironment.AzureUSGovernment };
@@ -23,25 +23,39 @@ async function main() {
     vms = await client.virtualMachines.listAll( );
     cache.cPut(cmdId, vms)
   }
-  // filter by name
-  if (program.name )  vms = vms.filter(e => e.name && e.name != program.name )
-  // filter the id by program.unit
-  if (program.unit) vms = vms.filter(e => e.id && new RegExp(program.unit).test(e.id))
+  let total = vms.length
+  // for Azure filter by name match
+  if ( program.Name )  vms = vms.filter(e =>  e.name == program.Name )
+  // for Azure filter the id by RegExp(program.unit)
+  if ( program.unit) vms = vms.filter(e => e.id && new RegExp(program.unit).test(e.id))
   
   console.log(JSON.stringify(vms,null,2));
+  if(program.verbose > 0 )  console.error(
+     `level-${program.verbose}: vm count: ${vms.length} of ${total} ${subject}s W/ filter :\n`,
+      (program.unit ? `, id by regexp /${program.unit}/\n` : ''),
+      (program.Name ? `, Name == ${program.Name}\n` : ''),
+      (program.verbose > 1 ? program : "")
+    )
 }
 var subject = "vm"
+function increaseVerbosity(v, total) {
+  return total + 1;
+}
 
 const program = require('commander')
 program
   .version(package.version)
-  .option('-D --debug', 'Debug messages')
-  .option(`-n --name <${subject}Name>`, `Specify ${subject} name`, "None")
-  .option(`-u --unit <${subject}Unit>`, 'Organizational parent-[container|filterRE|id|tag]', config.id)
-  .parse(process.argv)
+  .option(`-n --Name <${subject}Name>`, `Specify ${subject} Name`)
+  .option(`-u --unit <${subject}Unit>`, 'Organizational parent-container|filterRE|id')
+  .option('-v, --verbose', 'Verbose, repeat to increase log level', increaseVerbosity, 0)
+
+  //program.command("xx*").action( (c,o)=> {
+  //  console.error('Unknown command parameter: ', c)
+  //  process.exit(1)
+  //})
+  program.parse(process.argv)
 
 main( ).catch((err) => {
-      console.log("An error occurred: %O", err);    
+      console.error("An error occurred: %O", err);    
 })
-
 
