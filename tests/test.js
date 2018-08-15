2dca1b99e975844803e9f5dfb4f363da5695f49a
Steve <strong_stephen@bah.com>
2018-08-15 12:36:42 -0400

    small change

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,33 @@
 // Require the built in 'assertion' library
 var assert = require('assert');
-var prof = require('../lib/profiles')
-var data = require('../tests/profiles')
+var prof = require('../lib/profiles');
+var data = require('../tests/profiles');
 
 // Create a test suite (group) called Math
 describe('Roles x,y,z Profile Settings', function() {
-    // Test getRole x: 
-    it('x should return Profiles: default', function(){
-      assert.equal(prof.getRole('subject','x',data).ResolvedProfiles.join(), 'default');
-      });
-    // Test role y
-    it('y should return Profiles: b,c,d,default', function(){
-        assert.equal(prof.getRole('subject','y',data).ResolvedProfiles.join(), 'b,c,d,default');        assert.equal(prof.getRole('subject','y',data).ResolvedProfiles.join(), 'b,c,d,default');
-    });
-        // Test role z
-    it('z should return Profiles: c,b,default', function(){
-        assert.equal(prof.getRole('subject','z',data).ResolvedProfiles.join(), 'c,b,default');
-
-    });
-
+  // Test getRole x:
+  it('x should return Profiles: default', function() {
+    assert.equal(
+      prof.getRole('subject', 'x', data).ResolvedProfiles.join(),
+      'default'
+    );
+  });
+  // Test role y
+  it('y should return Profiles: b,c,d,default', function() {
+    assert.equal(
+      prof.getRole('subject', 'y', data).ResolvedProfiles.join(),
+      'b,c,d,default'
+    );
+    assert.equal(
+      prof.getRole('subject', 'y', data).ResolvedProfiles.join(),
+      'b,c,d,default'
+    );
+  });
+  // Test role z
+  it('z should return Profiles: c,b,default', function() {
+    assert.equal(
+      prof.getRole('subject', 'z', data).ResolvedProfiles.join(),
+      'c,b,default'
+    );
+  });
 });
-
