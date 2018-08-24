22aadc10b7558aa5ea465c2667bae86c0885fa83
Dan Jones <djones.home@comcast.net>
2018-08-24 04:04:30 -0700

    Inorder to validate settings, on load,  moved into Async function miain.

--- bin/ccfoo.js
+++ bin/ccfoo.js
@@ -3,25 +3,30 @@
 const fs = require('fs'); 
 const path = require("path");
 const cfg = require('../lib/settings')
-var config = cfg.load()
 const package = require('../package')
-//const subject = __filename.split('-').pop()
 
 var program = require('commander') 
- .version(package.version)
- 
+.version(package.version)
 
-// exec (external) commands
-var execCmds = [ "network", "vm", "storage", "security",  'user' ]
-execCmds.sort().forEach( n=>{
-  // name an exec command, like: ./bin/ccfoo-$n.js 
-  program.command(n, `Act on ${n} subjects`)
-})  
-
-// action (built-in) commands 
-program.command('config <cmd> [key] [value]')
-  .description( "Configure local settings: config [show|set <key value>|del <k>]")
-  .action( (cmd, k, v, options) => cfg.action(program, config, cmd, k, v) )
-
-program.parse(process.argv);
+// inorder to validate settings, on load, it needs to be in an Async functions, hence main.
+async function main() {
+   config = await cfg.load( )
+   
+  // exec (external) commands
+  var execCmds = [ "network", "vm", "storage", "security",  'user' ]
+  execCmds.sort().forEach( n => {
+    // name an exec command, like: ./bin/ccfoo-$n.js 
+    program.command(n, `Act on ${n} subjects`)
+  })  
+  
+  // action (built-in) commands 
+  program.command('config <cmd> [key] [value]')
+    .description( "Configure local settings: config [show|set <key value>|del <k>]")
+    .action( (cmd, k, v, options) => cfg.action(program, config, cmd, k, v) )
+  
+  program.parse(process.argv);
+}
+main( ).catch((err) => {
+  console.error("Error: %O", err);    
+})
 
