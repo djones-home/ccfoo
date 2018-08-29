2972a6e7c220fb240c243a96e71a273275df9bca
Steve <strong_stephen@bah.com>
2018-08-29 10:33:00 -0400

    add token and tests

---
+++ test/azureToken.spec.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+
+const log = console.log;
+
+const settings = require('../lib/settings')
+const tokenCashe = require("../lib/azureToken");
+
+
+describe('Test commands of azureToken.. ', () => {
+    //define the commands with the module
+
+  describe('the azure token cashe', () =>  {
+
+
+    it('should get azure creds for current profile...', () => {
+      const profile = settings.load();
+
+      tokenCashe.getCredentials(profile)
+      .then(credentials => {
+        assert(credentials);
+      })
+      .catch(err => {
+        log(err)
+      })
+    });
+
+  });
+});
