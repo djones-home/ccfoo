659d37bb72ed14ec8f174a8ba7f55084e86fcb69
Dan Jones <djones.home@comcast.net>
2018-09-04 15:39:15 -0700

    mostly makes a vm

--- test/azure.spec.js
+++ test/azure.spec.js
@@ -5,27 +5,79 @@ const log = console.log;
 
 const cfg = require('../lib/settings')
 const azure = require("../lib/azure");
+const cloud = { azure: azure }
 
 
 describe('Test commands of azure login.. ', () => {
-    //define the commands with the module
+  //define the commands with the module
 
-  describe('the azure login token cashe', () =>  {
+  describe('the azure login token cashe', () => {
 
 
     it('should get azure creds for current profile...', () => {
-      cfg.load().then(config => {
-        azure.getCreds(config)
-        .then(credentials => {
-          assert(credentials);
-        })
-        .catch(err => {
-          log(err)
-        })
+      cfg.load(cloud).then(config => {
+        azure.getCreds({ config })
+          .then(credentials => {
+            assert(credentials);
+          })
+          .catch(err => {
+            log(err)
+          })
+
+      })
+
+    });
+
+    // it('should get create a resource group...', () => {
+    //   let client;
+
+
+    //   cfg.load(cloud).then(config => {
+    //     assert(config);
+    //     azure.getCreds({ config })
+    //       .then(credentials => {
+    //         assert(credentials);
+    //         return azure.establishResourceGroup(credentials, config, "vm")
+    //       }).then(result => {
+    //         assert(result.group);
+    //       })
+    //       .catch(err => {
+    //         log(err)
+    //       })
+
+    //   })
+
+    // });
+
+
+
+
+    it('should deploy a template VM to  resource group...', () => {
+
+      cfg.load(cloud).then(config => {
+        assert(config);
+        azure.getCreds({ config })
+          .then(credentials => {
+            assert(credentials);
+            return azure.establishResourceGroup(credentials, config, "vm")
+          }).then(result => {
+            let { client, group } = result;
+            let template = require('../templates/arm/vm/template');
+            let parameters = {}
+            return azure.deployTemplate({ client, group, parameters, template });
+          })
+          .then(result => {
+            assert(result);
+          })
+          .catch(err => {
+            log(err)
+          })
 
       })
 
     });
 
   });
+
+
 });
